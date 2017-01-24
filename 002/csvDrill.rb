fc137b6e7768f9b19bd4cccf70546b1a226c47b7
Aaron Steward <aa.daly.stew@gmail.com>
2017-01-24 16:59:53 -0600

    First work with CSV drill.

--- 002/csvDrill.rb
+++ 002/csvDrill.rb
@@ -1,5 +1,10 @@
 require 'csv'
+total = []
+t = 0.0
 
 CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
-    puts row["Category"]
+    total.push(row["Outflow"].to_f)
 end
+
+puts total
+
