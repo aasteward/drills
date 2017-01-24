8dd86c880710e122e5afb776452bdce113622b52
Aaron Steward <aa.daly.stew@gmail.com>
2017-01-24 14:38:19 -0600

    Attempted to fix bug in loop counter incrementing.

--- 002/change-counter.rb
+++ 002/change-counter.rb
@@ -6,20 +6,23 @@ p = 0
 def changecount(money)
     money = money.round(2)
     while money > 0.24 do
-        q += 1
+        q = q + 1
         money -= 0.25
     end
     while money > 0.09 do
-        d += 1
+        d = d + 1
         money -= 0.10
     end
     while money > 0.04 do
-        n += 1
+        n = n + 1
         money -= 0.05
     end
     while money > 0.00 do
-        p += 1
+        p = p + 1
         money -= 0.01
     end
     return q + " quarters\n" + d + " dimes\n" + n + " nickel\n" + p + " pennies"
 end
+
+testInput = 4.17
+changecount(testInput)
