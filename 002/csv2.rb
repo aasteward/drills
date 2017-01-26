16c6b15f0f25667fc59f03ff38349834a7597b08
Aaron Steward <aa.daly.stew@gmail.com>
2017-01-26 16:45:05 -0600

    Re-write to use hash instead of millions of arrays.

--- 002/csv2.rb
+++ 002/csv2.rb
@@ -1,22 +1,45 @@
 require 'CSV'
 require 'pry'
 
-spending = []
-earning = []
-spent = 0.0
-earned = 0.0
-avspend = 0.0
-avearned = 0.0
+accounts = {}
+#Priya = {total => 0.0, {}}
+#Sonia = {total => 0.0, {}}
+#category = {}
 
 CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
-    if (row["Account"].include? "Sonia")
-    	puts row["Account"]
-   	    spending.push(row["Outflow"].split("$").last().sub(/,/, '').to_f)
-	    earning.push(row["Inflow"].split("$").last().sub(/,/, '').to_f)
+    account = row["Account"].chomp
 
+    if !accounts[account]
+    	accounts[account] = { :total => 0.0, :category => {}}
     end
+
+    current_account = accounts[account]
+
+    current_category = row["Category"].chomp
+
+    if !accounts[account][:category][current_category]
+    	accounts[account][:category][current_category] = { :tally => 0.0, :num_of_transactions => 0, :avg_transaction => 0.0 }
+#    	binding.pry
+    end
+
+inflow = row["Inflow"].gsub(/[,\$]/, '').to_f.round(2)
+
+outflow = row["Outflow"].gsub(/[,\$]/, '').to_f.round(2)
+
+amount = inflow - outflow
+
+current_account[:category][current_category][:tally] += amount
+current_account[:category][current_category][:num_of_transactions] += 1
+current_account[:category][current_category][:avg_transaction] = current_account[:category][current_category][:tally] / current_account[:category][current_category][:num_of_transactions]
+
+current_account[:total] += amount
+
+binding.pry
+
 
 end
 
-puts spending
-puts earning
+#binding.pry
+
+puts "Done"
+
--- 002/csvDrill.rb
+++ 002/csvDrill.rb
@@ -1,411 +1,109 @@
 require 'csv'
 require 'pry'
 
-sspending = []
-searning = []
-sspent = 0.0
-searned = 0.0
-savspend = 0.0
-savearned = 0.0
+#FUNCTION TO FIND ACCOUNT NAMES
 
-sallowout = []
-sallowin = []
-srepairout = []
-srepairin = []
-srentout = []
-srentin = []
-sfunout = []
-sfunin = []
-sfuelout = []
-sfuelin = []
-sgrocerout = []
-sgrocerin = []
-smealout = []
-smealin = []
-sclothesout = []
-sclothesin = []
-seducateout = []
-seducatein = []
-sdonateout = []
-sdonatein = []
-sutilityout = []
-sutilityin = []
-sgiftout = []
-sgiftin = []
-shouseout = []
-shousein = []
-sgymout = []
-sgymin = []
-smedicalout = []
-smedicalin = []
+def findNames(csvFile)
+	x = []
 
-pspending = []
-pearning = []
-pspent = 0.0
-pearned = 0.0
-pavspend = 0.0
-pavearned = 0.0
-
-pallowout = []
-pallowin = []
-prepairout = []
-prepairin = []
-prentout = []
-prentin = []
-pfunout = []
-pfunin = []
-pfuelout = []
-pfuelin = []
-pgrocerout = []
-pgrocerin = []
-pmealout = []
-pmealin = []
-pclothesout = []
-pclothesin = []
-peducateout = []
-peducatein = []
-pdonateout = []
-pdonatein = []
-putilityout = []
-putilityin = []
-pgiftout = []
-pgiftin = []
-phouseout = []
-phousein = []
-pgymout = []
-pgymin = []
-pmedicalout = []
-pmedicalin = []
-
-CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
-
-	# POPULATES ARRAYS WITH TRANSACTIONS FOR EACH CATEGORY FOR EACH ACCOUNT
-
-	if (row["Category"].include? "Allowance")
-		if (row["Account"].include? "Sonia")
-    		sallowout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		sallowin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			pallowout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		pallowin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
+	CSV.foreach(csvFile, {headers: true, return_headers: false}) do |row|
+		x.push(row["Account"].strip)
 	end
-
-	if (row["Category"].include? "Car Repairs")
-		if (row["Account"].include? "Sonia")
-    		srepairout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		srepairin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			prepairout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		prepairin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
-	if (row["Category"].include? "Rent")
-		if (row["Account"].include? "Sonia")
-    		srentout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		srentin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			prentout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		prentin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
-	if (row["Category"].include? "Entertainment")
-		if (row["Account"].include? "Sonia")
-    		sfunout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		sfunin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			pfunout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		pfunin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
-	if (row["Category"].include? "Fuel")
-		if (row["Account"].include? "Sonia")
-    		sfuelout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		sfuelin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			pfuelout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		pfuelin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
-	if (row["Category"].include? "Groceries")
-		if (row["Account"].include? "Sonia")
-    		sgrocerout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		sgrocerin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			pgrocerout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		pgrocerin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
-	if (row["Category"].include? "Meals")
-		if (row["Account"].include? "Sonia")
-    		smealout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		smealin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			pmealout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		pmealin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
-	if (row["Category"].include? "Clothes")
-		if (row["Account"].include? "Sonia")
-    		sclothesout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		sclothesin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			pclothesout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		pclothesin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
-	if (row["Category"].include? "Education")
-		if (row["Account"].include? "Sonia")
-    		seducateout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		seducatein.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			peducateout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		peducatein.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
-	if (row["Category"].include? "Donations")
-		if (row["Account"].include? "Sonia")
-    		sdonateout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		sdonatein.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			pdonateout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		pdonatein.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
-	if (row["Category"].include? "Utilities")
-		if (row["Account"].include? "Sonia")
-    		sutilityout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		sutilityin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			putilityout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		putilityin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
-	if (row["Category"].include? "Gifts")
-		if (row["Account"].include? "Sonia")
-    		sgiftout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		sgiftin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			pgiftout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		pgiftin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
-	if (row["Category"].include? "Household Goods")
-		if (row["Account"].include? "Sonia")
-    		shouseout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		shousein.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			phouseout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		phousein.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
-	if (row["Category"].include? "Gym")
-		if (row["Account"].include? "Sonia")
-    		sgymout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		sgymin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			pgymout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		pgymin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
-	if (row["Category"].include? "Medical/Dental")
-		if (row["Account"].include? "Sonia")
-    		smedicalout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		smedicalin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-		end
-		if (row["Account"].include? "Priya")
-			pmedicalout.push(row["Outflow"].split("$").last().sub(/,/, '').to_f) if row["Outflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    		pmedicalin.push(row["Inflow"].split("$").last().sub(/,/, '').to_f) if row["Inflow"].split("$").last().sub(/,/, '').to_f != 0.0
-    	end
-	end
-
+	return x.uniq
 end
 
-# CALCULATES SONIA'S TRANSACTION TOTALS FOR EACH CATEGORY
+#FUNCTION TO PULL ALL CATEGORIES FROM FILE
 
-sallowtot = 0.0
-sallowin.each { |t| sallowtot += t }
-sallowout.each { |t| sallowtot -= t }
+def findCats(csvFile)
+	x = []
 
-srepairtot = 0.0
-srepairout.each { |t| srepairtot -= t }
-srepairin.each { |t| srepairtot += t }
+	CSV.foreach(csvFile, {headers: true, return_headers: false}) do |row|
+    	x.push(row["Category"].strip)
+    end
+ return x.uniq
+end
 
-srenttot = 0.0
-srentout.each { |t| srenttot -= t }
-srentin.each { |t| srenttot += t }
+#FUNCTION THAT FINDS ALL IN/OUTFLOW OF GIVEN PARAMETERS
 
-sfuntot = 0.0
-sfunout.each { |t| sfuntot -= t }
-sfunin.each { |t| sfuntot += t }
+def toatlAndAverageCalc(category, person)
 
-sfueltot = 0.0
-sfuelout.each { |t| sfueltot -= t }
-sfuelin.each { |t| sfueltot += t }
+	totIn = []
+	totOut = []
 
-sgrocertot = 0.0
-sgrocerout.each { |t| sgrocertot -= t }
-sgrocerin.each { |t| sgrocertot += t }
+	CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
+		if row["Category"].include? category
+			if row["Account"].include? person
+				totIn.push(row["Inflow"].gsub(/[,\$]/, "").to_f) if row["Inflow"].gsub(/[,\$]/, "").to_f != 0.0
+				totOut.push(row["Outflow"].gsub(/[,\$]/, "").to_f) if row["Outflow"].gsub(/[,\$]/, "").to_f != 0.0
+			end
+		end
+	end
 
-smealtot = 0.0
-smealout.each { |t| smealtot -= t }
-smealin.each { |t| smealtot += t }
+	numOfTransactions = totIn.length + totOut.length
+	total = 0.0
+	average = 0.0
 
-sclothestot = 0.0
-sclothesout.each { |t| sclothestot -= t }
-sclothesin.each { |t| sclothestot += t }
+	totIn.each { |i| total += i }
+	totOut.each{ |i| total -= i }
 
-seducatetot = 0.0
-seducateout.each { |t| seducatetot -= t }
-seducatein.each { |t| seducatetot += t }
+	if numOfTransactions == 0
+		average = "No occurance"
+	else
+		average = (total/numOfTransactions).round(2) 
+	end
 
-sdonatetot = 0.0
-sdonateout.each { |t| sdonatetot -= t }
-sdonatein.each { |t| sdonatetot += t }
+	return person, category, total.round(2), average
+end
 
-sutilitytot = 0.0
-sutilityout.each { |t| sutilitytot -= t }
-sutilityin.each { |t| sutilitytot += t }
+#BEGINING OF CODE EXECUTION
 
-sgifttot = 0.0
-sgiftout.each { |t| sgifttot -= t }
-sgiftin.each { |t| sgifttot += t }
+names = findNames('accounts.csv')
+categories = findCats('accounts.csv')
 
-shousetot = 0.0
-shouseout.each { |t| shousetot -= t }
-shousein.each { |t| shousetot += t }
+priyaarr = []
+soniaarr = []
 
-sgymtot = 0.0
-sgymout.each { |t| sgymtot -= t }
-sgymin.each { |t| sgymtot += t }
+names.each do |n|
+	categories.each do |c|
+		priyaarr.push(toatlAndAverageCalc(c,n)) if n == "Priya"
+		soniaarr.push(toatlAndAverageCalc(c,n)) if n == "Sonia" 
+	end
+end
 
-smedicaltot = 0.0
-smedicalout.each { |t| smedicaltot -= t }
-smedicalin.each { |t| smedicaltot += t }
+data = [soniaarr, priyaarr]
 
-# CALCULATES PRIYA'S TRANSACTION TOTALS FOR EACH CATEGORY
 
-pallowtot = 0.0
-pallowin.each { |t| pallowtot += t }
-pallowout.each { |t| pallowtot -= t }
 
-prepairtot = 0.0
-prepairout.each { |t| prepairtot -= t }
-prepairin.each { |t| prepairtot += t }
 
-prenttot = 0.0
-prentout.each { |t| prenttot -= t }
-prentin.each { |t| prenttot += t }
+names.each do |n|
+	if (n.to_s.include? "Sonia")
+		b = soniaarr.each do |t|
+			t += sr[2]
+		end
 
-pfuntot = 0.0
-pfunout.each { |t| pfuntot -= t }
-pfunin.each { |t| pfuntot += t }
+	end
+	if (n.to_s.include? "Priya")
+		b = priyaarr.each do |t|
+			t += pr[2]
+		end
+		
+	end
+	puts "================================================================================"
+	puts "#{n}... 		Balance: #{b}"
+	puts "================================================================================"
+	puts "Category                    | Total spent | Average per transaction"
+	puts "----------------------------|-------------|-------------------------------------"
+	if n.to_s.include? "Sonia"
+		soniaarr.each do |sr|
+			print "#{sr[1].ljust(29)}" + "#{sr[2].to_s.ljust(14)}" + "#{sr[3].to_s.ljust(37)}" + "\n" unless sr.include? "No occurance"
+		end
+	end
+	if n.to_s.include? "Priya"
+		priyaarr.each do |pr|
+			print "#{pr[1].ljust(29)}" + "#{pr[2].to_s.ljust(14)}" + "#{pr[3].to_s.ljust(37)}" + "\n" unless pr.include? "No occurance"
+		end
+	end
+	puts "\n\n"
 
-pfueltot = 0.0
-pfuelout.each { |t| pfueltot -= t }
-pfuelin.each { |t| pfueltot += t }
-
-pgrocertot = 0.0
-pgrocerout.each { |t| pgrocertot -= t }
-pgrocerin.each { |t| pgrocertot += t }
-
-pmealtot = 0.0
-pmealout.each { |t| pmealtot -= t }
-pmealin.each { |t| pmealtot += t }
-
-pclothestot = 0.0
-pclothesout.each { |t| pclothestot -= t }
-pclothesin.each { |t| pclothestot += t }
-
-peducatetot = 0.0
-peducateout.each { |t| peducatetot -= t }
-peducatein.each { |t| peducatetot += t }
-
-pdonatetot = 0.0
-pdonateout.each { |t| pdonatetot -= t }
-pdonatein.each { |t| pdonatetot += t }
-
-putilitytot = 0.0
-putilityout.each { |t| putilitytot -= t }
-putilityin.each { |t| putilitytot += t }
-
-pgifttot = 0.0
-pgiftout.each { |t| pgifttot -= t }
-pgiftin.each { |t| pgifttot += t }
-
-phousetot = 0.0
-phouseout.each { |t| phousetot -= t }
-phousein.each { |t| phousetot += t }
-
-pgymtot = 0.0
-pgymout.each { |t| pgymtot -= t }
-pgymin.each { |t| pgymtot += t }
-
-pmedicaltot = 0.0
-pmedicalout.each { |t| pmedical -= t }
-pmedicalin.each { |t| pmedical += t }
-
-# savspent = sspent / sspending.length
-savallow = sallowtot / ( sallowin.length + sallowout.length )
-puts savallow
-
-pavallow = pallowtot / ( pallowin.length + pallowout.length )
-puts pavallow
-
-# puts "Sonia spent average #{savspent}"
-
-# searning -= [0.0]
-# #puts searning
-
-# searning.each { |e| searned += e}
-# puts "Sonia earned total #{searned}"
-
-# savearned = searned / searning.length
-# puts "Sonia earned average #{savearned}"
-
-# pspending -= [0.0]
-# #puts pspending
-
-# pspending.each { |s| pspent += s}
-# puts "Priya spent total #{pspent}"
-
-# pavspent = pspent / pspending.length
-
-# puts "Priya spent average #{pavspent}"
-
-# pearning -= [0.0]
-# #puts pearning
-
-# pearning.each { |e| pearned += e}
-# puts "Priya earned total #{pearned}"
-
-# pavearned = pearned / pearning.length
-# puts "Priya earned average #{pavearned}"
+end
 
