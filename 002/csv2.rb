ad5fc394bdba8d7dbdded67c45e56b33883e9854
Aaron Steward <aa.daly.stew@gmail.com>
2017-01-27 10:36:33 -0600

    Finalized display. Added ability to filter report by account name. 'csv2.rb' is now master copy.

--- 002/csv2.rb
+++ 002/csv2.rb
@@ -32,24 +32,27 @@ current_account[:category][current_category][:tally] += amount
 current_account[:category][current_category][:num_of_transactions] += 1
 current_account[:category][current_category][:avg_transaction] = current_account[:category][current_category][:tally] / current_account[:category][current_category][:num_of_transactions]
 
+# CALCULATES ACCOUNT BALANCE
+
 current_account[:total] += amount
 
 end
 
 # CREATES DISPLAY
 
+if ARGV[0].to_s != ''
+	accounts.delete_if { |key, value| key != ARGV[0].to_s }
+end
+
 accounts.each do |name, balance|
 	puts "==============================================================="
-	puts "#{name}: 		Balance: $#{balance[:total].round(2)}"
+	puts "  #{name}: 		Balance: \$#{balance[:total].round(2)}"
 	puts "==============================================================="
 	puts "| Category 		| Amount 	| Average Transaction |"
 	puts "| --------------------- | ------------- | ------------------- |"
 	balance[:category].each do |category, t|
     	print "| #{category.ljust(21)} | \$#{t[:tally].round(2).to_s.ljust(12)} | \$#{t[:avg_transaction].round(2).to_s.ljust(18)} |\n"
 	end
-
 end
 
-
-puts "Done"
 
