6c3d68f14b2ed2dd9eb99a8e32026f8cfc54d7a6
Aaron Steward <aa.daly.stew@gmail.com>
2017-01-30 14:57:25 -0600

    Updated 'csv2.rb' to object oriented file.

--- 002/csv2.rb
+++ 002/csv2.rb
@@ -1,124 +1,127 @@
-require "CSV"
-require "pry"
+require "csv"
+require 'pry'
 
-accounts = {}
-filter = ARGV
+class AccountInfo
+  def set_up_initial_values
+    @tally = 0.00
+    @categories = {}
+  end
 
-# BUILD ACCOUNT CLASS WITH OPERATIONS
+  def update_tally(amount)
+    @tally += amount
+  end
 
-class Account
-	def initialize
-		@total = 0.0
-		@categories = {}
-	end
-	def balance
-		@total += amount
-	end
-	def add_category
-		@categories[category] = Category.new
-		@categories[category] = initialize
-	end
-	def existing_category(category)
-		return @categories[category] != nil
-	end
-	def category(category)
-		return @categories[category]
-	end
-	def categories
-		return @categories
-	end
+  def add_category(category_name)
+    @categories[category_name] = Category.new
+    @categories[category_name].set_up_initial_values
+  end
+
+  def pretty_tally
+    return @tally.round(2)
+  end
+
+  def already_has_category(category_name)
+    return (@categories[category_name] != nil)
+  end
+
+  def category(category_name)
+    return @categories[category_name]
+  end
+
+  def categories
+    return @categories
+  end
 end
-
-# BUILD CATEGORY CLASS WITH OPERATIONS
 
 class Category
-	def initialize
-		@tally = 0.0
-		@num_of_transactions = 0
-		@avg_transaction = @tally / @num_of_transactions
-	end
-	def track(amount)
-		@tally += amount
-	end
-	def tally
-		@tally.round(2).to_s
-	end
-	def average
-		@avg_transactions.round(2).to_s
-	end
+  def set_up_initial_values
+    @tally = 0.00
+    @num_transactions = 0
+    @average_transaction_cost = 0.00
+  end
+
+  def add_transaction(amount)
+    @tally += amount
+    @num_transactions += 1
+    @average_transaction_cost = @tally / @num_transactions
+  end
+
+  def pretty_tally
+    @tally.round(2).to_s.ljust(10)
+  end
+
+  def pretty_avg_transaction
+    @average_transaction_cost.round(2).to_s.ljust(20)
+  end
 end
 
-# BUILD INFLOW/OUTFLOW CLASSES
-# MAYBE MERGE TO SINGLE CLASS
-
 class Outflow
-	def value(amount)
-		@value = amount.gsub(/[,\$]/, '').to_f.round(2)
-	end
-	def to_f
-		return @value
-	end
+  def set_value(number_string_from_csv)
+    @value = number_string_from_csv.gsub(/[,\$]/, "").to_f.round(2)
+  end
+
+  def to_f
+    return @value
+  end
 end
 
 class Inflow
-	def value(amount)
-		@value = amount.gsub(/[,\$]/, '').to_f.round(2)
-	end
-	def to_f
-		return @value
-	end
+  def set_value(number_string_from_csv)
+    @value = number_string_from_csv.gsub(/[,\$]/, "").to_f.round(2)
+  end
+
+  def to_f
+    return @value
+  end
 end
 
-# SEARCH CSV FOR DATA
+accounts = {}
 
 CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
+  # Add a key for each account to the accounts Hash.
+  account = row["Account"].chomp
 
-	# CREATES NEW ACCOUNTS
+  if !accounts[account]
+    accounts[account] = AccountInfo.new
+    accounts[account].set_up_initial_values
+  end
 
-    account = row["Account"].chomp
-    if !accounts[account]
-    	accounts[account] = Account.new.initialize
-    end
+  # Set the account which is being affected by this iteration.
+  current_account = accounts[account]
 
-    if !accounts[account].existing_category
-    	accounts[account].add_category
+  # Clean up outflow and inflow.
+  outflow = Outflow.new
+  outflow.set_value(row["Outflow"])
+  inflow = Inflow.new
+  inflow.set_value(row["Inflow"])
+  
+  transaction_amount = inflow.to_f - outflow.to_f
 
-    inflow = Inflow.new.value(row["Inflow"]).to_f
-    outflow = Outflow.new.value(row["Outflow"]).to_f
-    amount = inflow - outflow
-    # ^ CAN BE MERGED?
+  # Keep a tally for current balance of the account.
+  current_account.update_tally(transaction_amount)
 
+  category = row["Category"].chomp
 
-# QUICK ATTEMPT AT DIRECT TEMPLATE CONVERSTION TO FUNCTIONS
+  # Initialize category.
+  if !current_account.already_has_category(category)
+    current_account.add_category(category)
+  end
 
-# def new_accounts(names)
-# 	account = row["Account"].chomp
-# 	if !accounts[account]
-#     	accounts[account] = { :total => 0.0, :category => {}}
-#     end
-#     current_account = accounts[account]
-# end
+  # Add transaction for that category.
+  current_account.category(category).add_transaction(transaction_amount)
+end
 
-# def categories(types)
-# 	current_category = row["Category"].chomp
-# 	if !accounts[account][:category][current_category]
-#     	accounts[account][:category][current_category] = { :tally => 0.0, :num_of_transactions => 0, :avg_transaction => 0.0 }
-#     end
-# end
+#  Display
 
-# def transaction(amount)
-# 	inflow = row["Inflow"].gsub(/[,\$]/, '').to_f.round(2)
-# 	outflow = row["Outflow"].gsub(/[,\$]/, '').to_f.round(2)
-# 	amount = inflow - outflow
-# end
-
-# def tracker(books)
-# 	current_account[:category][current_category][:tally] += amount
-# 	current_account[:category][current_category][:num_of_transactions] += 1
-# 	current_account[:category][current_category][:avg_transaction] = current_account[:category][current_category][:tally] / current_account[:category][current_category][:num_of_transactions]
-# end
-
-# def update_balance(total)
-# 	current_account[:total] += amount
-# end
-
+accounts.each do |name, info|
+  puts "\n"
+  puts "======================================================================"
+  puts "Account: #{name}... Balance: $#{info.pretty_tally}"
+  puts "======================================================================"
+  puts "Category                     | Total Spent | Average Transaction"
+  puts "---------------------------- | ----------- | -------------------------"
+  info.categories.each do |category, c_info|
+    print "#{category.ljust(28)} | $#{c_info.pretty_tally} | $#{c_info.pretty_avg_transaction}\n"
+  end
+  puts "\n"
+end
