e48290bf71d283ed30b9e2883be5b733e686a446
Aaron Steward <aa.daly.stew@gmail.com>
2017-02-01 13:53:45 -0600

    Finished web display controllers.

--- 007/main.rb
+++ 007/main.rb
@@ -3,12 +3,12 @@ require_relative './functions.rb'
 require "pry"
 require "csv"
 
-def display(n, t)
-	n.set_up_initial_values
-	n.build_report(n, t)
+def display(name)
+	account = AccountInfo.new
+	account.set_up_initial_values
+	account.build_report(account, name)
+	return account
 
-	@name = t
-	@balance = n.pretty_tally
 end
 
 get ("/"){
@@ -16,44 +16,22 @@ get ("/"){
 }
 
 get("/sonia") {
-	@sonia = AccountInfo.new
-	display(@sonia, "Sonia")
+	@name = "Sonia"
+	@account = display(@name)
 	erb :sonia
 }
 
 get("/priya") {
-	@priya = AccountInfo.new
-	display(@priya, "Priya")
+	@name = "Priya"
+	@account = display(@name)
 	erb :priya
 }
 
 get("/full") {
-	@vars = ["@sonia", "@priya"]
 	@names = ["Sonia", "Priya"]
-	@i = 0
-	begin
-		@vars[i] = AccountInfo.new
-		display(@vars[i], @names[i])
-		@i++
-	# for (i = 0; i < @vars.length; i++) {
-	# 	@vars[i] = AccountInfo.new
-	# 	display(@vars[i], @names[i])
-	# }
-	end while @i < @vars.length
-
-	# @sonia = AccountInfo.new
-	# @sonia.set_up_initial_values
-	# @sonia.build_report(@sonia, "Sonia")
-
-	# @name1 = "Sonia"
-	# @balance1 = @sonia.pretty_tally
-	
-	# @priya = AccountInfo.new
-	# @priya.set_up_initial_values
-	# @priya.build_report(@priya, "Priya")
-
-	# @name2 = "Priya"
-	# @balance2 = @priya.pretty_tally
-
+	@all_accounts = []
+	@names.each do |account|
+		@all_accounts.push(display(account))
+	end
 	erb :full
 }
