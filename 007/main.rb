24fef59b163dcca20fb96cd1d4b6beb5e63ba32f
Aaron Steward <aa.daly.stew@gmail.com>
2017-02-02 15:56:32 -0600

    Fixed extra line breaks.

--- 007/main.rb
+++ 007/main.rb
@@ -13,7 +13,7 @@ end
 
 def write_info(new_info)
 	info = File.open("/Users/aasteward/Code/drills/007/accounts.csv", "a")
-	info.puts new_info
+	info.print new_info
 	info.close
 end
 
@@ -24,10 +24,10 @@ end
 
 post("/submit") {
 	info_array = []
-	info_array.push(params["account"],params["date"],params["payee"],params["category"],params["outflow"],params["inflow"])
+	info_array.push(params["account"], params["date"], params["payee"], params["category"], params["outflow"], params["inflow"])
 	new_info = "\n" + info_array.join(",")
-	account = params["account"]
 	write_info(new_info)
+	account = params["account"]
 	redirect("/report?name=#{account}")
 }
 
