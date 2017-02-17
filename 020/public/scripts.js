454b4b61caeb671fc840ea0b35e86d0e9cea8542
Aaron Steward <aa.daly.stew@gmail.com>
2017-02-17 15:00:31 -0600

    Page now dynamically generates data from JSON.

--- 020/public/scripts.js
+++ 020/public/scripts.js
@@ -1,42 +1,30 @@
-var population = [];
 window.addEventListener("load", function(){
 	// IMPORT JSON TO VAR 'POPULATION'
-	// var population = [];
+	var population = [];
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", "/people");
 	xhr.onload = function(){
 		population = xhr.responseText;
 		population = JSON.parse(population);
+		personList(population);
 	}
 	xhr.send();
-	console.log(population);
 
 	// GATHER DATA FOR EACH PERSON IN JSON
-	for (i = 0; i < population.length; i++){
-		var ID = population[i];
-		personData(ID);
+	function personList(population){
+		for (i = 0; i < population.length; i++){
+			var person = population[i];
+			personData(person);
+		}
 	}
 
-	// BUILD DIV ELEMENT AND APPEND TO HTML
-	function personData(p){
+	// BUILD DIV AND APPEND TO HTML
+	function personData(person){
 		var newDiv = document.createElement("div");
-		// DIV BUILDING GOES HERE
-		
+		var info = newDiv.innerHTML;
+		info = "First Name: " + person.fname + "<br>" + "Last Name: " + person.lname + "<br>" + "Phone: " + person.tel + "<br>" + "Address: " + person.address + "<br>" + "City: " + person.city + "<br>" + "State: " + person.state + "<br>" + "ZIP: " + person.zip + "<br><br>";
+		newDiv.innerHTML = info;
 		document.getElementsByTagName("body")[0].appendChild(newDiv);
 	}
 
-	// MAYBE NOT NECESSARY??
-	// function personList(){
-
-	// }
-
-
-	// TEST CODE TO BUILD DIV ON PAGE
-// var mydiv = document.createElement('div');
-// mydiv.style.width = "100px";
-// mydiv.style.height = "100px";
-// mydiv.style.background = "red";
-// mydiv.style.color = "white";
-// mydiv.innerHTML = "Hello";
-// document.getElementsByTagName("body")[0].appendChild(mydiv);
 });
